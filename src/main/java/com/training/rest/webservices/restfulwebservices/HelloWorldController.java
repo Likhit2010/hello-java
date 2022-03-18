e90b994911c3d2dd75ea27d0c12a0cd801f84b17
Likhit2010 <rao.javaprof@gmail.com>
2022-03-18 19:56:10 +0530

    First commmit

---
+++ src/main/java/com/training/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.training.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/")
+	public String hello() {
+		return "{\"message\":\"Hello Java v1\"}";
+	}
+}
